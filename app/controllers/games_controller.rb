36a02eb50d4726dc7e6003345d4bf97923017a32
karin knutsson <karinmiriam@gmail.com>
2021-04-11 22:10:10 +0200

    added functionality to count score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -36,15 +36,29 @@ class GamesController < ApplicationController
     false
   end
 
+  def compute_score(letters)
+    score = 0
+    letters.each do |l|
+      score += 1 if l.scan(/[AEILNORSTU]/i).length.positive?
+      score += 2 if l.scan(/[DG]/i).length.positive?
+      score += 3 if l.scan(/[BCMP]/i).length.positive?
+      score += 4 if l.scan(/[FHVWY]/i).length.positive?
+      score += 5 if l.scan(/K/i).length.positive?
+      score += 8 if l.scan(/[JX]/i).length.positive?
+      score += 10 if l.scan(/[QZ]/i).length.positive?
+    end
+    score
+  end
+
   def score
     letters = params[:letters].split(' ')
     solution = params[:solution].upcase.split('')
     match = match?(solution, letters)
     real_word = in_dictionary?(solution.join.downcase)
     @result = { letters: letters, solution: solution }
-    return @result[:score] = 2 if match && real_word
-    return @result[:score] = 1 if match
+    return @result[:score] = compute_score(letters) if match && real_word
+    return @result[:score] = -1 if match
 
-    @result[:score] = 0
+    @result[:score] = -2
   end
 end
